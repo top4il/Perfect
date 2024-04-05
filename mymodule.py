3cacf352bd704da1c803617288124bd5e7ce5a8f
Dmitriy Seskutov <xerox0707@gmail.com>
2024-04-05 14:18:38 +0700

    third commit

--- mymodule.py
+++ mymodule.py
@@ -1,5 +1,9 @@
+# Home work 
+# Home work 
+# Home work 
+
 def my_function(first, second):
-    return first * second + 10
+    return first + second + 10
 
 def another_function(first):
     return first + 1
